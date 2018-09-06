d2185ee08e40a02bc59749d9204ec4666a9a7790
honkiitong <info@buecker-it.de>
2018-09-06 18:53:44 +0200

    added powerline fonts to deps.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@
 # Base setup
 sudo apt-get install build-essential arandr cmake dunst dzen2 exfat-utils \
     git herbstluftwm  meld nmap redshift rxvt-unicode-256color suckless-tools \
-    tig tmux xfonts-terminus profanity pwgen nitrogen \
+    tig tmux xfonts-terminus fonts-powerline profanity pwgen nitrogen \
     python-pygments \
     help2man xclip
 
